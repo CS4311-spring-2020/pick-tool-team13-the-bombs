5a4c44a4f81cee9d34fbe51759c80653dda21ef9
Victor Vargas <vevargascor@miners.utep.edu>
2020-02-29 15:25:25 -0700

    Added button behavior in main window

---
+++ PICK Logic/PY FIles/logicMainWindow.py
@@ -0,0 +1,57 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import (QApplication,QWidget, QFormLayout,QCheckBox, QGroupBox, QWidget,QLineEdit,QDialogButtonBox, QLabel, QMainWindow, QAction, qApp, QPushButton, QDialog,QApplication, QCheckBox, QComboBox, QDateTimeEdit,
+        QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
+        QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
+        QVBoxLayout, QWidget, QStyle, QDialogButtonBox, QTableWidgetItem)
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+from PICKGUI import Ui_MainWindow
+from filter import filterPopup
+from icons import IconConfigDialog
+from Event import Ui_EventConfig
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        
+        self.centWid = self.findChild(QtWidgets.QWidget,'centralwidget')
+        self.filtButt = self.centWid.findChild(QtWidgets.QPushButton,'LogEntryFilterBut')
+        self.filtButt.clicked.connect(self.showFilter)
+        self.graphViewIconButt = self.centWid.findChild(QtWidgets.QPushButton, 'GraphViewIconConfigBut')
+        self.doubViewIconButt = self.centWid.findChild(QtWidgets.QPushButton, 'DoubleViewIconConfigBut')
+        self.graphViewIconButt.clicked.connect(self.showIcons)
+        self.doubViewIconButt.clicked.connect(self.showIcons)
+
+        self.eventConfigButt = self.findChild(QtWidgets.QAction,'actionEvent_Configuration_2')
+        self.eventConfigButt.triggered.connect(self.showEventConfig)
+        
+
+    #Method to show filter popup
+    def showFilter(self):
+        exPopup = filterPopup(self)
+        exPopup.show()
+
+    def showIcons(self):
+        exPopup = IconConfigDialog(self)
+        exPopup.show()
+
+    def showEventConfig(self):
+        self.EventConfig = QtWidgets.QWidget()
+        ui = Ui_EventConfig()
+        ui.setupUi(self.EventConfig)
+        self.EventConfig.show()
+
+    
+
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec()
